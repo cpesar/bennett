532858dd471a382262b4d7927db55d8ee1e5cea5
cpesar <cpesar1@gmail.com>
2024-06-29 23:23:08 -0600

    handled form validation with zod

--- src/components/topics/topic-create-form.tsx
+++ src/components/topics/topic-create-form.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useFormState } from "react-dom";
+
 import {
   Input,
   Button,
@@ -9,6 +13,9 @@ import {
 import * as actions from "@/actions";
 
 export default function TopicCreateForm() {
+  const [formState, action] = useFormState(actions.createTopic, {
+    errors: {},
+  });
   return (
     <Popover placement="left">
       <PopoverTrigger>
@@ -16,15 +23,30 @@ export default function TopicCreateForm() {
       </PopoverTrigger>
 
       <PopoverContent>
-        <form action={actions.createTopic}>
+        <form action={action}>
           <div className="flex flex-col gap-4 p-4 w-80">
             <h3 className="text-lg">Create a Topic</h3>
-            <Input label="Name" labelPlacement="outside" placeholder="Name" />
+            <Input
+              name="name"
+              label="Name"
+              labelPlacement="outside"
+              placeholder="Name"
+              isInvalid={!!formState.errors.name}
+              errorMessage={formState.errors.name?.join(", ")}
+            />
             <Textarea
+              name="description"
               label="Description"
               labelPlacement="outside"
               placeholder="Describe your topic"
+              isInvalid={!!formState.errors.description}
+              errorMessage={formState.errors.description?.join(", ")}
             />
+            {formState.errors._form ? (
+              <div className="p-2 bg-red-200 border border-red-400 rounded">
+                {formState.errors._form.join(", ")}
+              </div>
+            ) : null}
             <Button type="submit">Submit</Button>
           </div>
         </form>
